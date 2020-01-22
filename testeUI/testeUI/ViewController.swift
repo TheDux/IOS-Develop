351d8365d3af2113745d43395510a63f360e243c
TheDux <tiago_afm95@hotmail.com>
2020-01-22 15:33:21 -0300

    IMC app

---
+++ testeUI/testeUI/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  testeUI
+//
+//  Created by TIAGO MARTINS on 19/01/20.
+//  Copyright © 2020 Tiago Martins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var viImg: UIImageView!
+    @IBOutlet weak var viResults: UIView!
+    var imc: Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func calcnaofunfa(_ sender: Any) {
+        if let weight = Double(tfWeight.text!), let height = Double (tfHeight.text!){
+            imc = weight / (height*height)
+            showResults()
+        }
+    }
+    /*@IBAction func calculate(_ sender: Any) {
+        print("default")
+        if let weight = Double(tfWeight.text!), let height = Double (tfHeight.text!){
+            imc = weight / (height*height)
+            showResults()
+        }
+        
+    }*/
+    
+    func showResults(){
+        var result: String = " "
+        var image: String = " "
+        switch imc {
+        case 0..<16:
+            result = "Magreza"
+            image = "magreza.png"
+        case 16..<18.5:
+            result = "Abaixo do peso"
+            image = "abaixo.png"
+        case 18.5..<25:
+            result = "Peso ideal"
+            image = "ideal.png"
+        case 25..<30:
+            result = "sobrepeso"
+            image = "sobre.png"
+        default:
+            result = "Obesidade"
+            image = "obesidade.png"
+        }
+        viResults.isHidden = false
+        lbResult.text = result
+        
+       self.viImg.image =  UIImage(named: image)
+        
+    }
+
+}
+
